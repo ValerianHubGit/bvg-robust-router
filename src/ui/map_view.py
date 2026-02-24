843e4ec017c8db84c6119288e245156611805bf0
valerianhubgit <VKurowski@gmx.de>
2026-02-24 08:25:01 +0100

    edit: Kommentarergänzung map_view.py

---
+++ src/ui/map_view.py
@@ -0,0 +1,6 @@
+"""Überbleibsel initial geplanter Projektstruktur
+Bei selbst gebautem ganzheitlichen BVG Netz als gerichtetem Graphen hätten wir diesen,
+sowie die Strecke visualisiert.
+Da die BVG API allerdings fertige Journeys von Start zum Ziel ausgibt, 
+erschien dies unverhältnismäßig und zur API unpassend. 
+"""
